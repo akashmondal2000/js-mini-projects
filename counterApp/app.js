a88ce7291c43a1ad5c584a94a36a45bc66ebf83a
akashmondal2000 <mondalakash494@gmail.com>
2024-04-19 16:10:42 +0530

    increment and decrement by user given number and reset

--- counterApp/app.js
+++ counterApp/app.js
@@ -1,16 +1,24 @@
 const plusButton = document.querySelector(".plus-btn");
 const minusButton = document.querySelector('.minus-btn');
 const count = document.querySelector('.count');
+const changeBy = document.querySelector('.change-by');
+const resetButton = document.querySelector('.reset-btn');
 
 
 const increment = ()=>{
+    const changeByValue = parseInt(changeBy.value) ;
     
-   count.innerText = parseInt(count.innerText) + 1;
+   count.innerText = parseInt(count.innerText) + changeByValue;
 }
 const decrement = () =>{
-    count.innerText = parseInt(count.innerText) - 1;
+    const changeByValue = parseInt(changeBy.value);
+    count.innerText = parseInt(count.innerText) - changeByValue;
+}
+const resetCount = () =>{
+    count.innerText = 0;
 }
 
 
 plusButton.addEventListener("click",increment);
 minusButton.addEventListener('click',decrement);
+resetButton.addEventListener('click',resetCount);
