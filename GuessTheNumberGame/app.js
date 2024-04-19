625742c0de0252d52df673dd4150adac25e40177
akashmondal2000 <mondalakash494@gmail.com>
2024-04-19 19:44:34 +0530

    implement ifie and finish the game

--- GuessTheNumberGame/app.js
+++ GuessTheNumberGame/app.js
@@ -1,29 +1,43 @@
-const userInput = document.querySelector('.guess-input');
-const form = document.querySelector('form');
-const result = document.querySelector('.result');
-const allGuesses = document.querySelector('.all-guesses');
-const allGuessesArray = [];
+// ifie stands for immedietly innvoke function;
+;(() => {
+  const userInput = document.querySelector(".guess-input");
+  const form = document.querySelector("form");
+  const result = document.querySelector(".result");
+  const allGuesses = document.querySelector(".all-guesses");
+  const submitButton = document.querySelector(".submit-btn");
+  const startGameButton = document.querySelector(".startGame-btn");
 
-// this randem number generate ramdom whole number between 0 to 100; 
-let randomNumber = Math.round(Math.random()*100);
+  const allGuessesArray = [];
 
-form.addEventListener('submit',(e)=>{
+  // this randem number generate ramdom whole number between 0 to 100;
+  let randomNumber = Math.round(Math.random() * 100);
+
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const userInputValue = parseInt(userInput.value);
     // console.log('Submitted')
-    if(randomNumber === userInputValue){
-        result.innerText =  'Congrats';
-    } else if ( userInputValue > randomNumber){
-       result.innerText= 'Too High';
-    }else {
-         result.innerText ='Too Low!';
+    if (randomNumber === userInputValue) {
+      result.innerText = "You got it! Congrats";
+      startGameButton.disabled = false;
+      submitButton.disabled = true;
+    } else if (userInputValue > randomNumber) {
+      result.innerText = "Too High";
+    } else {
+      result.innerText = "Too Low!";
     }
     allGuessesArray.push(userInputValue);
-    
-    allGuesses.innerText ='Your Guesses: '+ allGuessesArray.join(', ')
 
+    allGuesses.innerText = "Your Guesses: " + allGuessesArray.join(", ");
 
-    // userInput.value = ''; // for clear the input box 
-    form.reset()//for clear the input box this is an another approch
+    // userInput.value = ''; // for clear the input box
+    form.reset(); //for clear the input box this is an another approch
+  });
 
-})
+  startGameButton.addEventListener("click", () => {
+    result.innerText = "";
+    allGuesses.innerText = "";
+    startGameButton.disabled = true;
+    submitButton.disabled = false;
+    randomNumber = Math.round(Math.random() * 100);
+  });
+})();
