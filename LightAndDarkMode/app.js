973675303333ef676d486657db52aa629a88d3d9
akashmondal2000 <mondalakash494@gmail.com>
2024-04-23 22:46:36 +0530

    add container dark mode feture and change button style

--- LightAndDarkMode/app.js
+++ LightAndDarkMode/app.js
@@ -1,10 +1,21 @@
-const fullDerkMode = document.getElementById('full-derk-mode');
+const fullDerkMode = document.getElementById("full-derk-mode");
 
+const containerDarkMode = document.getElementById("contained-derk-mode");
 
-fullDerkMode.addEventListener('change',()=>{
-    if(fullDerkMode.checked){
-        document.body.classList.add('dark');
-    }else{
-        document.body.classList.remove('dark')
-    }
-})
+const Container = document.querySelector(".container");
+
+fullDerkMode.addEventListener("change", () => {
+  if (fullDerkMode.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
+
+containerDarkMode.addEventListener("change", () => {
+  if (containerDarkMode.checked) {
+    Container.classList.add("dark");
+  } else {
+    Container.classList.remove("dark");
+  }
+});
